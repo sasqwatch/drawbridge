8a5a8a15fdaadba23884ce800326e711c834ebed
woachk <mmediouni@gmx.fr>
2018-10-07 17:26:49 +0200

    More changes

--- buildrunner.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Script for changing the configuration file for the Drawbridge PAL.
-
-objcopy --add-section .config=config.ini --set-section-flags .config=contents,readonly palrun.elf runner
