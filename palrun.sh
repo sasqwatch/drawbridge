c1a3a12b59d95a132ddd79811e24bb51c48a0708
woachk <mmediouni@gmx.fr>
2018-10-07 17:25:18 +0200

    Script change

---
+++ palrun.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Script for changing the configuration file for the Drawbridge PAL.
+
+objcopy --add-section .config=config.ini --set-section-flags .config=contents,readonly palrun.elf runner
+./runner "$@"
