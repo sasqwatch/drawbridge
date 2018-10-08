f6b9bfb49a0731743d64e6e1501e3ede3ceac6cc
woachk <mmediouni@gmx.fr>
2018-10-08 06:34:09 +0200

    Preparing expansion.

--- pal/micropal/objects.c
+++ pal/micropal/objects.c
@@ -1,6 +1,9 @@
 #include <pal.h>
 #include <unistd.h>
+#include "pal_private.h"
 
 void DkObjectClose (PAL_HANDLE objectHandle) {
-	close(objectHandle);
+	if (objectHandle->hdr.type == pal_type_file) {
+		close(((PAL_FILE_HANDLE)objectHandle)->descriptor);
+	}
 }
--- pal/micropal/streams.c
+++ pal/micropal/streams.c
@@ -2,10 +2,15 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <string.h>
+#include "pal_private.h"
+#include <stdlib.h>
 
 PAL_HANDLE DkStreamOpen (PAL_STR path, PAL_FLG flags, PAL_FLG share, PAL_FLG create_if_not_existing, PAL_FLG options) {
+	PAL_FILE_HANDLE handle = malloc(sizeof(struct pal_file_handle));
 	if (!strcmp("dev:tty", path) && flags == PAL_ACCESS_WRONLY) {
-		return 1; 
+		handle->hdr.type = pal_type_file;
+		handle->descriptor = 1;
+		return (PAL_HANDLE)handle; 
 	}
 	else {
 		return -1; // not implemented
@@ -15,5 +20,7 @@ PAL_HANDLE DkStreamOpen (PAL_STR path, PAL_FLG flags, PAL_FLG share, PAL_FLG cre
 PAL_NUM DkStreamWrite (PAL_HANDLE handle, PAL_NUM offset, PAL_NUM count, PAL_PTR buffer, PAL_STR dest) {
 	int8_t* buffer2 = buffer;
 	buffer2 = buffer2 + offset;
-	write(handle, buffer2, count);
+	if (handle->hdr.type == pal_type_file) {
+		write(((PAL_FILE_HANDLE)handle)->descriptor, buffer2, count);
+        }
 }
