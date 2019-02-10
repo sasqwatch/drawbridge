038dcaf910bf8a9014db8664ce9975d1f81a662e
woachk <24752637+woachk@users.noreply.github.com>
2019-02-10 16:49:38 -0500

    Tiny change

--- initial.sh
+++ initial.sh
@@ -2,7 +2,7 @@
 
 # Initialize environment.
 
-cp /opt/mssql/bin/palrun palrun.elf
+cp /opt/mssql/bin/sqlservr palrun.elf
 cp /opt/mssql/lib/*.so* .
 cp /opt/mssql/lib/*.sfp .
 strip --remove-section=.config palrun.elf
